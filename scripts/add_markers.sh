e2692bbb1d08b48bea5e6afe04eb09a0f6d49dec
Nataliya <nataliya.mishukova@gmail.com>
2021-05-25 19:20:58 +0000

    feat: Fix add_marker.sh to make marker disappear and appear at a different location

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -3,6 +3,11 @@ xterm -e " roslaunch my_robot world.launch" &
 sleep 30
 xterm -e " roslaunch my_robot amcl.launch" &
 sleep 10
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e " rosrun rviz rviz -d $(rospack find my_robot)/rviz/newRvizConfig.rviz" &
 sleep 10
-xterm -e " rosrun add_markers add_markers"
+xterm -e " rosrun add_markers add_markers" &
+sleep 5
+xterm -e " rosparam set pick_up_location_reached true" &
+sleep 5
+xterm -e " rosparam set drop_off_location_reached true"
+
